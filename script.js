049e84c647ff190b45c98321261bb6b963155eb1
souvlaki <110357841+GabrielLaplace@users.noreply.github.com>
2024-08-13 08:51:38 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,41 +1,23 @@
-document.getElementById('searchCifra').addEventListener('click', function() {
+document.getElementById('searchButton').addEventListener('click', function() {
     const query = document.getElementById('searchInput').value.trim();
     
     if (query) {
         const cifraClubUrl = `https://www.cifraclub.com.br/?q=${encodeURIComponent(query)}`;
-        document.getElementById('cifraClubLink').href = cifraClubUrl;
-        document.getElementById('cifraClub').style.display = 'block';
-    } 
-    else 
-    {
-        document.getElementById('cifraClub').style.display = 'none';
-    }
-});
-
-document.getElementById('searchSongster').addEventListener('click', function() {
-    const query = document.getElementById('searchInput').value.trim();
-    
-    if (query) {
         const songsterrUrl = `https://www.songsterr.com/?pattern=${encodeURIComponent(query)}`;
-        document.getElementById('songsterrLink').href = songsterrUrl;
-        document.getElementById('songsterr').style.display = 'block';
-    } 
-    else 
-    {
-        document.getElementById('songsterr').style.display = 'none';
-    }
-});
-
-document.getElementById('searchUltimate').addEventListener('click', function() {
-    const query = document.getElementById('searchInput').value.trim();
-       
-    if (query) {
         const ultimateGuitarUrl = `https://www.ultimate-guitar.com/search.php?search_type=title&value=${encodeURIComponent(query)}`;
+
+        document.getElementById('cifraClubLink').href = cifraClubUrl;
+        document.getElementById('songsterrLink').href = songsterrUrl;
         document.getElementById('ultimateGuitarLink').href = ultimateGuitarUrl;
+
+        document.getElementById('cifraClub').style.display = 'block';
+        document.getElementById('songsterr').style.display = 'block';
         document.getElementById('ultimateGuitar').style.display = 'block';
     } 
     else 
     {
+        document.getElementById('cifraClub').style.display = 'none';
+        document.getElementById('songsterr').style.display = 'none';
         document.getElementById('ultimateGuitar').style.display = 'none';
     }
 });
